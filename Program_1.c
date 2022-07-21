ac12dfe065d72f8614f0782bc3837a7a76cc0917
eshan.one <108228113+eshan-one@users.noreply.github.com>
2022-07-21 15:18:32 +0530

    Create Program_1.c

---
+++ Program_1.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+int main() {
+  int x,y;
+  for(y=1;y<=5;y++)
+  {
+    for(x=1;x<=5;x++){
+      if(x<=y){
+       printf(" * ");  
+      }
+      else{
+        printf(" ");
+      }
+   
+  }
+  printf("\n");
+    
+  }
+  printf("\n");
+
+  
+  
+  
+  
+  return 0;
+}
